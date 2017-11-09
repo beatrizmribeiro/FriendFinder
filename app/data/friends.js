21c6c1ed3e77c91f424a2ea56bb1d84ad5ea4e1c
Beatriz Ribeiro <beatriz_mribeiro@hotmail.com>
2017-11-09 01:17:44 -0800

    updated friends.js

---
+++ app/data/friends.js
@@ -0,0 +1,58 @@
+var friendArray = [
+{
+	"name":"Brian",
+	"photo":"http://4everstatic.com/pictures/674xX/people/musicians/brian-littrell,-red-rose-160445.jpg",
+	"scores":[5, 1, 4, 4, 5, 1, 2, 5, 4,1]
+},
+
+{
+	"name":"Nick",
+	"photo":"http://hauteliving.com/wp-content/uploads/2017/06/Nick-Press-Photo.jpg"
+	"scores":[4, 2, 5, 5, 1, 2, 3, 1, 5, 2]
+},
+
+{
+	"name": "AJ",
+	"photo": "http://cdn-img.instyle.com/sites/default/files/styles/684xflex/public/1473165679/090616-aj-mclean-lead.jpg?itok=WTAItDzz",
+	"scores": [1, 3, 1, 1, 2, 3, 4, 2, 1, 3]
+},
+{
+	"name": "Howie D",
+	"photo": "http://www.idolator.com/wp-content/uploads/sites/10/2012/01/Howie-D-Idolator-Favorites.jpg",
+	"scores": [2, 4, 2, 2, 3, 4, 5, 3, 2, 4]
+},
+{
+	"name": "Kevin",
+	"photo": "https://i1.wp.com/thepoppingpost.com/wp-content/uploads/2015/03/kevin.jpg",
+	"scores": [3, 5, 3, 3, 2, 3, 1, 4, 1, 5]
+},
+{
+	"name": "Justin",
+	"photo": "http://popblerd.com/wp-content/uploads/2013/10/JT-Nsync.jpg",
+	"score": [2, 2, 4, 4, 3, 1, 5, 5, 2, 1]
+},
+{
+	"name": "JC",
+	"photo": "https://i.pinimg.com/originals/99/4c/a8/994ca81faf3b6c5dbc2b2671c0d050c4.jpg",
+	"score": [1, 1, 1, 1, 1, 1, 1, 1, 1, 2]
+},
+{
+	"name": "Joey",
+	"photo": "http://www.billboard.com/files/styles/article_main_image/public/media/Joey-Fatone-2001-portrait-billboard-650.jpg",
+	"score": [5, 5, 5, 5, 5, 5, 5, 5, 5, 4]
+}, 
+{
+	"name": "Lance",
+	"photo": "https://img.buzzfeed.com/buzzfeed-static/static/campaign_images/web04/2012/9/21/12/lance-bass-champion-of-90s-style-1-32046-1348244432-0_big.jpg",
+	"score": [4, 4, 4, 3, 3, 3, 3, 2, 2, 1]
+},
+{
+	"name": "Chris",
+	"photo": "http://stillnsync.com/wp-content/uploads/2012/07/Screen-shot-2012-07-18-at-5.41.33-PM.png",
+	"score": [2, 2, 2, 2, 2, 2, 3, 4, 5, 4]
+};
+
+
+];
+
+module.exports = friends;
